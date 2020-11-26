40203de089bbe3e449cf08f7a8011398c5c4c22f
Hoang Tran <hoang.tran@terralogic.com>
2020-11-26 11:33:48 +0700

    Setup project

---
+++ NetworkManager/NetworkManager/NetworkManager.h
@@ -0,0 +1,18 @@
+//
+//  NetworkManager.h
+//  NetworkManager
+//
+//  Created by Hoang Tran on 11/26/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkManager.
+FOUNDATION_EXPORT double NetworkManagerVersionNumber;
+
+//! Project version string for NetworkManager.
+FOUNDATION_EXPORT const unsigned char NetworkManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkManager/PublicHeader.h>
+
+
